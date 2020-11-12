d9653e250c165634db1b0fbe6a760ddb28a5b60c
Engel <engelvip@list.ru>
2020-11-12 22:31:53 +0300

    ПР №4

---
+++ app/src/main/java/com/example/project4/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.project4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvInfo;
+    EditText etInput;
+    Button bControl;
+    int guess;
+    boolean gameFinished;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvInfo = (TextView)findViewById(R.id.textView2);
+
+        etInput = (EditText)findViewById(R.id.editText2);
+
+        bControl = (Button)findViewById(R.id.button2);
+
+        guess = (int)(Math.random()*100);
+        gameFinished = false;
+    }
+
+    public void onPush(View v){
+        if (!gameFinished) {
+            int inp = Integer.parseInt(etInput.getText().toString());
+            if (inp > guess)
+                tvInfo.setText(getResources().getString(R.string.ahead));
+            if (inp < guess)
+                tvInfo.setText("Недолет!");
+            if (inp > 100)
+                tvInfo.setText("Больше 100 нельзя");
+            if (inp < 1)
+                tvInfo.setText("Меньше 1 нельзя");
+            if (inp == guess) {
+                tvInfo.setText("В точку");
+                bControl.setText(getResources().getString(R.string.play_more));
+            }
+
+        }
+        else
+        {guess = (int)Math.random()*100;
+            bControl.setText("Ввести значение");
+            tvInfo.setText(getResources().getString(R.string.try_to_guess));
+            gameFinished=false;
+
+        }
+
+    }
+}
